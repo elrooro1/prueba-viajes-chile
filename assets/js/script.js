472b41abd695973f4a6fb20965ca82321c6c00c8
rodrigo gonzalez <rodrigogonzalezo197@gmail.com>
2024-02-03 14:20:53 -0300

    se le puso alerta de enviado correcto al boton enviar formulario luego se le puso funcion desaparecer textos alalas cartas y desaparecen la cartas en version mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,14 @@ $(document).ready(function(){
 });
 
 
+function enviarMensaje() {
+    alert("Mensaje enviado con éxito");
+    
+}
+
+$(document).ready(function () {
+    $(".card-clickable").click(function () {
+        // Oculta solo el texto dentro de la card al hacer clic
+        $(this).find('.card-text').hide();
+    });
+});
